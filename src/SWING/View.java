aca67b3a4961a4aa0a82bc600701891bd603c123
show5116 <yyj161091@gmail.com>
2021-07-14 15:59:04 +0900

    firty commit

---
+++ src/SWING/View.java
@@ -0,0 +1,75 @@
+package SWING;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class View extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View frame = new View();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public View() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new CardLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "name_106820209479800");
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(123, 95, 115, 36);
+		panel.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				if(textField.getText().equals("aa")){
+					System.out.println("안녕하세요");
+				}
+			}
+		});
+		textField.setBounds(91, 33, 100, 21);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		rdbtnNewRadioButton.setBounds(57, 170, 121, 23);
+		panel.add(rdbtnNewRadioButton);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, "name_106825352407200");
+	}
+}
